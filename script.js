a1f8581807807671e86021f3b6624e7541a641c6
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-27 22:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+// Function to generate a list item
+let ol=getElementById("infi-list")
 
+function generateListItem() {
+  // Code to generate a list item
+     let newListItem = document.createElement("li");
+	ol.append("newListItem")
+	
+}
+
+// Generate 10 list items by default
+for (let i = 0; i < 10; i++) {
+  generateListItem();
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    // User has scrolled to the bottom of the page, generate 2 more list items
+    generateListItem();
+    generateListItem();
+  }
+});
